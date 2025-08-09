54ed45498040b01065ec683b008db49fa63d30d5
me-suzy <me.suzana@gmail.com>
2025-08-10 02:38:04 +0300

    Add phpMyNewsletter_v0.6.12/include/footeradm.inc.php

---
+++ phpMyNewsletter_v0.6.12/include/footeradm.inc.php
@@ -0,0 +1,14 @@
+	
+
+
+	</td>
+  </tr>
+</table>
+
+<p>&nbsp;</p>
+<HR width="60%" size=0>
+<center><a href=http://gregory.kokanosky.free.fr/phpmynewsletter/><img src=img/logo.png border=0></A><br>
+</center>
+</body>
+
+</html> 
