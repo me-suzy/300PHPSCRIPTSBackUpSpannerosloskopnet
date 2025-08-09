06e2e2e167fd7f252e7a8a2a659b9adb03d30be4
me-suzy <me.suzana@gmail.com>
2025-08-10 02:37:52 +0300

    Add phpMyNewsletter_v0.6.12/include/chooselang.php

---
+++ phpMyNewsletter_v0.6.12/include/chooselang.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN">
+<html>
+<head>
+  <title>phpMyNewsletter</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+ </head>
+<body>
+
+
+
+<h1>phpMyNewsletter v 0.6.11</h1>
+Choose your language :<BR> <FORM ACTION="customize.php" METHOD="POST"><CENTER>
+<SELECT NAME="l" TABINDEX=2>
+<OPTION VALUE="lang-french.php">Français</OPTION>
+<OPTION VALUE="lang-en.php">English</OPTION>
+<OPTION VALUE="lang-deutch.php">Deutch</OPTION>
+<OPTION VALUE="lang-it.php">Italiano</OPTION>
+<OPTION VALUE="lang-spanish.php">Spanish</OPTION>
+<OPTION VALUE="lang-slovak.php">Slovack</OPTION>
+<OPTION VALUE="lang-dutch.php">Dutch</OPTION>
+</SELECT>
+ <INPUT TYPE="submit" VALUE="ok">
+</FORM></CENTER>
+
+
+
+</body></html>
